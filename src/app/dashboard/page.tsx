a050fda1ebb52588bbbe9bbfae64d6f3524cfe13
Vipin K <kvipin99@gmail.com>
2026-03-10 17:53:08 +0000

    Branches - dashboard - remove breakdown expenditure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -123,12 +123,14 @@ export default function DashboardPage() {
       </div>
 
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-5">
-        <div className="lg:col-span-3">
+        <div className={isAdmin ? "lg:col-span-3" : "lg:col-span-5"}>
            <VehicleValidityAlerts />
         </div>
-        <div className="lg:col-span-2">
-          <ExpensesChart />
-        </div>
+        {isAdmin && (
+          <div className="lg:col-span-2">
+            <ExpensesChart />
+          </div>
+        )}
       </div>
     </div>
   );
