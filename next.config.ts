485b325915cceb17fb10fba0e3c801e42ed49ceb
Vipin K <kvipin99@gmail.com>
2026-03-15 05:28:44 +0000

    Try fixing this error: `Console Error: Body exceeded 1 MB limit. To conf

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   serverActions: {
-    bodySizeLimit: '500mb',
+    bodySizeLimit: '1gb',
   },
   images: {
     remotePatterns: [
